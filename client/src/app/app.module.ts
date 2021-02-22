fb1960e9ecdb02415b447da1334fe0f4d1902a60
Tuan Ngyen <nguyenthanhtuan032@outlook.com>
2021-02-22 09:52:35 +0700

    End of section 3

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
+    // BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
