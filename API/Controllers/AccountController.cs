d095fd6e11b18f67ae4fde56ba1fdc8a491be694
Tuan Thanh Nguyen <tnguyen546@dxc.com>
2021-02-25 09:25:32 +0700

    changed the object returned when user login or register in AccountController, register a fake token section on Developer server

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,13 +13,15 @@ namespace API.Controllers
     public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
-        public AccountController(DataContext context)
+        private readonly ITokenService _tokenService;
+        public AccountController(DataContext context, ITokenService tokenService)
         {
+            _tokenService = tokenService;
             _context = context;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register([FromBody] RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await UserExist(registerDto.Username)) return BadRequest("Username is taken");
             using var hmac = new HMACSHA512();
@@ -33,11 +36,15 @@ namespace API.Controllers
             _context.AppUsers.Add(newUser);
             await _context.SaveChangesAsync();
 
-            return newUser;
+            return new UserDto
+            {
+                Username = newUser.UserName,
+                Token = _tokenService.CreateToken(newUser)
+            };
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login([FromBody] LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.AppUsers.FirstOrDefaultAsync(m => m.UserName == loginDto.Username.ToLower());
             if (user == null) return Unauthorized("Invalid username");
@@ -50,7 +57,11 @@ namespace API.Controllers
                 if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
             }
 
-            return user;
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
         private async Task<bool> UserExist(string username)
